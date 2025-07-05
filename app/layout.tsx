de6891fae48c2caf92be73293895057ce04d317d
BebidasON2025 <contato.bebidason@gmail.com>
2025-07-05 10:14:27 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Cardápio Bebidas ON',
+  description: 'Created by GV Software',
+  generator: 'gvsoftware.tech',
 }
 
 export default function RootLayout({
